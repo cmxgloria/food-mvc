2f5f2d366557befd845be17dedd15b871b4dfe2a
Gloria Chen <cmxgloria@gmail.com>
2020-02-15 23:17:28 +1100

    updated

--- db/shared.rb
+++ db/shared.rb
@@ -1,7 +1,7 @@
 require 'pg'
 
 def run_sql(sql, args = [])
-  conn = PG.connect(dbname: 'goodfoodhunting')
+  conn = PG.connect(dbname: 'foodmvc')
   results = conn.exec_params(sql, args)
   conn.close
   results
